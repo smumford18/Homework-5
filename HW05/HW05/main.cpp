55ab603f6aaeff81f010c3dc85745402326d49c4
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-10-19 20:10:14 -0700

    no message

--- HW05/HW05/main.cpp
+++ HW05/HW05/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int analyzeInput(int *x);
 
 int main() {
     
-    
+    int x = 5;
+    int *ptr= NULL;
+    ptr = &x;
+    analyzeInput(ptr);
     return 0;
 }
+
+int analyzeInput(int *x)
+{
+    int *y;
+    srand((unsigned int)time(NULL));
+    for(int i=0; i < *x; i++) {
+        y[i] = rand() % 20;
+    }
+    int sum = 0;
+    for(int i=0; i < *x; i++) {
+        sum += y[i];
+    }
+    cout << sum << endl;
+    
+    return sum;
+}
+
