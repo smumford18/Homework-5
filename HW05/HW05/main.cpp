1f3b86a6b859e0de16f03f53abdee98f5e2c34b6
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-10-19 18:41:50 -0700

    Initial Program Create

---
+++ HW05/HW05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW05
+//
+//  Created by Steven Mumford on 10/19/16.
+//  Copyright © 2016 Steven Mumford. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    
+    return 0;
+}
